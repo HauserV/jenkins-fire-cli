b691089ef06bf51d9c40236e3d59fb1611cd1681
weihong.xu <xuweihong.cn@gmail.com>
2022-09-14 09:13:10 +0800

    feat: download necessary jar during init

--- jenkins_fire_cli/main.py
+++ jenkins_fire_cli/main.py
@@ -1,34 +1,44 @@
 from .config import Config
 from fire import Fire
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve, urlopen
 import os.path
+import xml.etree.ElementTree as ET
+
 
 class Commands:
 
     def __init__(self, config: Config):
         self.config = config
-    
+
     def init(self, jenkins_cli_url: str = None, jenkins_job_dsl_core_url: str = None, force_download=False):
         self._download_job_dsl_core(jenkins_job_dsl_core_url, force_download)
         self._download_jenkins_cli(jenkins_cli_url, force_download)
-    
+
+    def _get_latest_job_dsl_core_version(self):
+        url = 'https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/maven-metadata.xml'
+        with urlopen(url) as fp:
+            tree = ET.parse(fp)
+        return tree.find('./versioning/release').text
+
     def _download_job_dsl_core(self, url: str = None, force_download=False):
-        # TODO: read latest version from https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/maven-metadata.xml
-        url = url or 'https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/1.18/job-dsl-core-1.18-standalone.jar'
+        # TODO: read latest version from
+        version = self._get_latest_job_dsl_core_version()
+        url = url or 'https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/{version}/job-dsl-core-{version}-standalone.jar'.format(
+            version=version)
         target = self.config.job_dsl_core_path
         self._download_file(url, target, force_download)
-    
+
     def _download_jenkins_cli(self, url: str = None, force_download=False):
         url = url or self.config.jenkins_cli_download_url
         target = self.config.jenkins_cli_path
         self._download_file(url, target, force_download)
-        
+
     def _download_file(self, url: str, target: str, force_download: bool):
         if not force_download and os.path.exists(target):
             return
         print('Download {} to {} ...'.format(url, target))
         urlretrieve(url, target)
-        
+
 
 if __name__ == '__main__':
     config = Config()
